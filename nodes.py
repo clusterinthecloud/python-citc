ce5efeb80ede85add78476efeb9d8c24d0e09588
Matt Williams <matt@milliams.com>
2019-12-18 12:11:33 +0000

    Add beginnings of Node class

--- nodes.py
+++ nodes.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 import pathlib
-from typing import Iterator
+import subprocess
+from typing import Iterator, Type
 
 
 def node_list(slurm_conf: pathlib.Path) -> Iterator[str]:
@@ -12,7 +13,43 @@ def node_list(slurm_conf: pathlib.Path) -> Iterator[str]:
         for line in conf:
             if line.startswith("NodeName="):
                 nodelist = line.split()[0][9:]
-                if '[' in nodelist:
+                if "[" in nodelist:
                     # TODO use pyslurm.hostlist().create()/get_list()
                     pass
                 yield from nodelist.split(",")
+
+
+class Node:
+    def __init__(self):
+        pass
+
+    sinfo_fields = [
+        "nodelist",
+        "statelong",
+        "reason",
+        "cpus",
+        "socketcorethread",
+        "memory",
+        "features",
+        "gres",
+        "nodeaddr",
+        "timestamp",
+    ]
+
+    statelong: str
+
+    @classmethod
+    def from_name(cls: Type["Node"], nodename: str) -> "Node":
+        field_width = 40
+        sinfo_format = ",".join(f"{f}:{field_width}" for f in cls.sinfo_fields)
+        out = subprocess.run(
+            ["sinfo", "--nodes", nodename, "--Format", sinfo_format, "-h"]
+        ).stdout.decode()
+        fields = [
+            out[start : start + field_width].strip()
+            for start in range(0, len(out), field_width)
+        ]
+        data = {k: v for k, v in zip(cls.sinfo_fields, fields)}
+        n = cls()
+        n.statelong = data["statelong"]
+        return n
--- test_nodes.py
+++ test_nodes.py
@@ -1,6 +1,7 @@
+import subprocess
 import textwrap
 
-from nodes import node_list
+from nodes import node_list, Node
 
 
 def test_slurm_node_list(tmp_path):
@@ -27,3 +28,28 @@ def test_slurm_node_list(tmp_path):
     assert len(r) == 10
     assert r[0] == "t3-micro-0001"
     assert r[-1] == "t3-micro-0010"
+
+
+def test_create_node(mocker):
+    node_data = {
+        "nodelist": "vm-standard-e2-2-ad3-0001",
+        "statelong": "idle~",
+        "reason": "none",
+        "cpus": "4",
+        "socketcorethread": "1:2:2",
+        "memory": "13500",
+        "features": "shape=VM.Standard.E2.2,ad=3",
+        "gres": "(null)",
+        "nodeaddr": "vm-standard-e2-2-ad3-0001",
+        "timestamp": "Unknown",
+    }
+    sinfo_output = "".join(f"{node_data[f]:40}" for f in Node.sinfo_fields)
+    mocker.patch(
+        "subprocess.run",
+        return_value=subprocess.CompletedProcess(
+            args="", returncode=0, stdout=sinfo_output.encode()
+        ),
+    )
+    n = Node.from_name("test")
+
+    assert n.statelong == node_data["statelong"]
