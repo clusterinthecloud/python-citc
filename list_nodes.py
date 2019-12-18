5b25c4c0a6f03bb4cbd707f606f8d83dfd695ba9
Matt Williams <matt@milliams.com>
2019-12-18 10:50:58 +0000

    Support more complicated node lists

--- list_nodes.py
+++ list_nodes.py
@@ -3,6 +3,13 @@
 
 def slurm_node_list(slurm_conf):
     with open(slurm_conf) as conf:
+    """
+    Given a config file, gives all the nodes listed within
+    """
         for line in conf:
             if line.startswith("NodeName="):
-                yield line.split()[0][9:]
+                nodelist = line.split()[0][9:]
+                if '[' in nodelist:
+                    # TODO use pyslurm.hostlist.create/get_list
+                    pass
+                yield from nodelist.split(",")
