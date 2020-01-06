eba4bd02f9e45d62f0e674be9f654a01e8c037b6
Matt Williams <matt@milliams.com>
2020-01-06 13:58:39 +0000

    Start work on AWS support

---
+++ aws.py
@@ -0,0 +1,62 @@
+from typing import Type
+
+import boto3  # type: ignore
+
+
+class NodeNotFound(Exception):
+    pass
+
+
+def ec2_client(nodespace: dict):
+    return boto3.client("ec2", region_name=nodespace["region"])
+
+
+class AwsNode:
+    name: str
+    state: str
+
+    @classmethod
+    def from_name(
+        cls: Type["AwsNode"], nodename: str, client, nodespace: dict
+    ) -> "AwsNode":
+        instances = client.describe_instances(
+            Filters=[
+                {"Name": "tag:Name", "Values": [nodename]},
+                {"Name": "tag:cluster", "Values": [nodespace["cluster_id"]]},
+                {"Name": "tag:type", "Values": ["compute"]},
+            ]
+        )
+        # TODO check for multiple returned matches
+        if instances["Reservations"]:
+            instance = instances["Reservations"][0]["Instances"][0]
+        else:
+            raise NodeNotFound(f"{nodename}")
+
+        return cls.from_response(instance)
+
+    @classmethod
+    def from_response(cls: Type["AwsNode"], response) -> "AwsNode":
+        state = response["State"]["Name"]
+        name = next(pair["Value"] for pair in response["Tags"] if pair["Key"] == "Name")
+
+        n = cls()
+
+        n.name = name
+        n.state = state
+
+        return n
+
+
+def all_nodes(client, nodespace: dict):
+    result = client.describe_instances(
+        Filters=[
+            {"Name": "tag:cluster", "Values": [nodespace["cluster_id"]]},
+            {"Name": "tag:type", "Values": ["compute"]},
+        ]
+    )
+    if result["Reservations"]:
+        instances = result["Reservations"][0]["Instances"]
+    else:
+        instances = []
+
+    return [AwsNode.from_response(instance) for instance in instances]
---
+++ test_aws.py
@@ -0,0 +1,54 @@
+import boto3  # type: ignore
+import pytest  # type: ignore
+from aws import AwsNode, all_nodes
+from moto import mock_ec2  # type: ignore
+
+
+@pytest.fixture
+def nodespace():
+    return {
+        "cluster_id": "cluster1",
+    }
+
+
+@pytest.fixture(scope="function")
+def ec2():
+    with mock_ec2():
+        yield boto3.client("ec2", region_name="eu-west-1")
+
+
+def launch_node(nodename: str, client, nodespace):
+    tag_specifications = [
+        {
+            "ResourceType": "instance",
+            "Tags": [
+                {"Key": "Name", "Value": nodename},
+                {"Key": "cluster", "Value": nodespace["cluster_id"]},
+                {"Key": "type", "Value": "compute"},
+            ],
+        },
+    ]
+
+    client.run_instances(
+        ImageId="ami-760aaa0f",
+        TagSpecifications=tag_specifications,
+        MinCount=1,
+        MaxCount=1,
+    )
+
+
+def test_awsnode(ec2, nodespace):
+    launch_node("foo", ec2, nodespace)
+    node = AwsNode.from_name("foo", ec2, nodespace)
+    assert node.name == "foo"
+    assert node.state == "running"
+
+
+def test_all_nodes_empty(ec2, nodespace):
+    assert all_nodes(ec2, nodespace) == []
+
+
+def test_all_nodes(ec2, nodespace):
+    launch_node("foo", ec2, nodespace)
+    nodes = all_nodes(ec2, nodespace)
+    assert len(nodes) == 1
