f789de2662d8830a399ce9fb2e52477f400ff867
Matt Williams <matt@milliams.com>
2020-01-07 16:02:52 +0000

    Add test for empty node lists

--- tests/test_watchdog.py
+++ tests/test_watchdog.py
@@ -4,6 +4,15 @@ from citc.slurm import SlurmNode
 from citc.watchdog import crosscheck
 
 
+def test_crosscheck_empty():
+    slurm_nodes = []
+    cloud_nodes = []
+
+    res = crosscheck(slurm_nodes, cloud_nodes)
+    res = list(res)
+    assert not res
+
+
 def test_crosscheck_one_match():
     slurm_nodes = [SlurmNode(name="foo-1", state="idle", state_flag=None, features={})]
     cloud_nodes = [AwsNode(name="foo-1", state=NodeState.RUNNING)]
