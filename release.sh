85169cbd9c5bb8bd2c386f1b777d4bd4d8ca2cab
Matt Williams <matt@milliams.com>
2020-01-07 14:16:17 +0000

    Add confirm step to release script

--- release.sh
+++ release.sh
@@ -26,8 +26,18 @@ if ! git diff --cached --quiet
     exit 1
 fi
 
+echo "Are you sure you want to release the next ${version_spec} version?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) make install; break;;
+        No ) exit;;
+    esac
+done
+
+# Bump version in pyproject.toml
 poetry version "${version_spec}"
 new_version=$(poetry version | awk '{print $2}')
+
 git add pyproject.toml
 git commit -m "Update to version ${new_version}"
 git tag "${new_version}"
