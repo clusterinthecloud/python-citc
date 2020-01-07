05a25cdfc1e9ec2de3d87ba358f9c77ec2bcc0af
Matt Williams <matt@milliams.com>
2020-01-07 14:37:08 +0000

    Add test for idle node being off

--- tests/test_watchdog.py
+++ tests/test_watchdog.py
@@ -20,3 +20,12 @@ def test_missing_node_down(mocker):
     run = mocker.patch("subprocess.run")
     crosscheck(slurm_nodes, cloud_nodes)
     run.assert_called_once()
+
+
+def test_idle_node_off(mocker):
+    slurm_nodes = [SlurmNode(name="foo-1", state="idle", state_flag='~', features={})]
+    cloud_nodes = []
+
+    crosscheck(slurm_nodes, cloud_nodes)
+
+    # TODO assert that nothing hapenns
