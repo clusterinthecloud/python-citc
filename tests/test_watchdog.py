1fdfbf3ce9dddc9eef5825d5703f394e17918639
Matt Williams <matt@milliams.com>
2021-01-27 10:43:48 +0000

    Format for black

--- tests/test_watchdog.py
+++ tests/test_watchdog.py
@@ -17,7 +17,9 @@ def test_crosscheck_one_match():
     slurm_nodes = [
         SlurmNode(name="foo-1", state="idle", state_flag=None, features={}, reason="")
     ]
-    cloud_nodes = [AwsNode(name="foo-1", state=NodeState.RUNNING, ip="10.0.0.25", id="i-foobar")]
+    cloud_nodes = [
+        AwsNode(name="foo-1", state=NodeState.RUNNING, ip="10.0.0.25", id="i-foobar")
+    ]
 
     res = crosscheck(slurm_nodes, cloud_nodes)
     res = list(res)
